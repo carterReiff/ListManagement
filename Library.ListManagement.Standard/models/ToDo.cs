75a4baa0ccaa0a636d013ec952d2c411c187dde0
carter <carter@DESKTOP-H5GH0SS>
2022-06-29 18:02:30 -0400

    Add project files.

---
+++ Library.ListManagement.Standard/models/ToDo.cs
@@ -0,0 +1,38 @@
+﻿using Library.ListManagement.Standard.DTO;
+using ListManagement.interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListManagement.models
+{
+    public class ToDo: Item
+    {
+        public DateTime Deadline { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} {Description} Completed: {IsCompleted}";
+        }
+
+        public ToDo()
+        {
+
+        }
+
+        public ToDo(ToDoDTO dto)
+        {
+            Deadline = dto.Deadline;
+            IsCompleted = dto.IsCompleted;
+
+            Name = dto.Name;
+            Description = dto.Description;
+
+            Id = dto.Id;
+        }
+    }
+}
